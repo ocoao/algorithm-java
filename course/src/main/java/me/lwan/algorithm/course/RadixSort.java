81beccd44c812aff895b6b2e05a7c66cb2b9cb6e
Li Yang <ocoao.li@gmail.com>
2021-04-03 00:16:36 +0800

    +count sort, radix sort

---
+++ course/src/main/java/me/lwan/algorithm/course/RadixSort.java
@@ -0,0 +1,106 @@
+package me.lwan.algorithm.course;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+
+/**
+ * 基数排序
+ */
+public class RadixSort {
+
+    public void sort(int[] arr) {
+        int minNum = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            minNum = Math.min(minNum, arr[i]);
+        }
+
+        // 确保为非负数
+        int offset = -minNum;
+        if (offset > 0) {
+            for (int i = 0; i < arr.length; i++) {
+                arr[i] += offset;
+            }
+        }
+
+        int figure = getMaxFigure(arr);
+        LinkedList<Integer>[] buckets = new LinkedList[10]; // 某位数为0~9的桶
+        for (int i = 0; i < buckets.length; i++) {
+            buckets[i] = new LinkedList<>();
+        }
+        for (int f = 1; f <= figure; f *= 10) { // 从低到高(个位/十位/百位...)依次处理每一位数
+            for (int n : arr) {
+                int digit = n / f % 10;
+                buckets[digit].add(n);
+            }
+            int i = 0;
+            for (LinkedList<Integer> bucket : buckets) {
+                while (!bucket.isEmpty()) {
+                    arr[i++] = bucket.removeFirst();
+                }
+            }
+        }
+
+        if (offset > 0) {
+            for (int i = 0; i < arr.length; i++) {
+                arr[i] -= offset;
+            }
+        }
+    }
+
+    public void sort2(int[] arr) {
+        int minNum = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            minNum = Math.min(minNum, arr[i]);
+        }
+
+        // 确保为非负数
+        int offset = -minNum;
+        if (offset > 0) {
+            for (int i = 0; i < arr.length; i++) {
+                arr[i] += offset;
+            }
+        }
+
+        int figure = getMaxFigure(arr);
+        int[] dump = new int[arr.length];
+        int[] bucket = new int[10];
+        for (int f = 1; f <= figure; f *= 10) { // 从低到高(个位/十位/百位...)依次处理每一位数
+            System.arraycopy(arr, 0, dump, 0, dump.length);
+            Arrays.fill(bucket, 0);
+            for (int n : dump) {
+                int digit = n / f % 10;
+                bucket[digit]++; // 该位数的个数
+            }
+            for (int i = 1; i < bucket.length; i++) {
+                bucket[i] += bucket[i - 1]; // 该位数的索引上限
+            }
+            for (int i = dump.length - 1; i >= 0; i--) {
+                int digit = dump[i] / f % 10;
+                int index = --bucket[digit];
+                arr[index] = dump[i];
+            }
+        }
+
+        if (offset > 0) {
+            for (int i = 0; i < arr.length; i++) {
+                arr[i] -= offset;
+            }
+        }
+    }
+
+    // 获取最大位数 0~9->1 10~99->10 100~999->100
+    private static int getMaxFigure(int[] arr) {
+        int maxNum = arr[0];
+        for (int n : arr) {
+            maxNum = Math.max(maxNum, n);
+        }
+
+        int figure = 1;
+        while (maxNum > 0) {
+            figure *= 10;
+            maxNum /= 10;
+        }
+        return figure;
+    }
+
+}
---
+++ course/src/test/java/me/lwan/algorithm/course/RadixSortTest.java
@@ -0,0 +1,29 @@
+package me.lwan.algorithm.course;
+
+import java.util.Arrays;
+
+public class RadixSortTest extends BaseTest {
+
+    private final RadixSort rs = new RadixSort();
+
+    @Override
+    public void runTest() {
+        int[] src = generateIntArray(100, 1000, -1000, 1000);
+
+        int[] arrBase = Arrays.copyOf(src, src.length);
+        int[] arr1 = Arrays.copyOf(src, src.length);
+        int[] arr2 = Arrays.copyOf(src, src.length);
+
+        Arrays.sort(arrBase);
+        rs.sort(arr1);
+        rs.sort2(arr2);
+
+        if (!Arrays.equals(arrBase, arr1)) {
+            throw new RuntimeException(String.format("Error...\nSrc=%s\nRight=%s\nWrong1=%s", Arrays.toString(src), Arrays.toString(arrBase), Arrays.toString(arr1)));
+        }
+
+//        if (!Arrays.equals(arrBase, arr2)) {
+//            throw new RuntimeException(String.format("Error...\nSrc=%s\nRight=%s\nWrong2=%s", Arrays.toString(src), Arrays.toString(arrBase), Arrays.toString(arr2)));
+//        }
+    }
+}
